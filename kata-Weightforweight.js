5de5249f5ed8e4ec142bdf4388aea59faea45f9e
Altynbek Quat <65371350+altynbeq@users.noreply.github.com>
2022-05-30 17:53:34 +0200

    Update kata-Weightforweight.js

--- kata-Weightforweight.js
+++ kata-Weightforweight.js
@@ -13,4 +13,4 @@ function orderWeight(string) {
 it("Basic tests",function() {
     Test.assertEquals(orderWeight("103 123 4444 99 2000"), "2000 103 123 4444 99")
     Test.assertEquals(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"), "11 11 2000 10003 22 123 1234000 44444444 9999")
-})})
+})})`/
