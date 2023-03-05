f2c729465fbfd67709f8590fa1e8d10ef352ec71
Altynbek Quat <65371350+altynbeq@users.noreply.github.com>
2023-03-05 23:11:32 +0600

    codeWars Break camelCase
    6 kyu

---
+++ Break-camelCase.js
@@ -0,0 +1,11 @@
+// // Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(string) {
+  string =  string.replace(/([A-Z])/g, ' $1');
+  return string
+}
