b7e840dbedaa5b026974b9016d4b29429b3e06be
Altynbek Quat <65371350+altynbeq@users.noreply.github.com>
2023-10-15 01:08:24 +0200

    Create PersonalizedMessage.c
    8 kyu
    Grasshopper - Personalized Message

---
+++ C/PersonalizedMessage.c
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/c
+
+const char* greet(const char *name, const char *owner) {
+  return strcmp(name, owner)? "Hello guest" : "Hello boss";
+}
